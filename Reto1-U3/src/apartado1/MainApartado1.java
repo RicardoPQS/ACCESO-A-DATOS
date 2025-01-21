3c5fad71e905dd86e6ca363be795601c94d06282
RicardoPQS <152377540+RicardoPQS@users.noreply.github.com>
2025-01-21 13:09:10 +0100

    Update MainApartado1.java

--- Reto1-U3/src/apartado1/MainApartado1.java
+++ Reto1-U3/src/apartado1/MainApartado1.java
@@ -7,7 +7,9 @@ public class MainApartado1 {
 
 	public static void main(String[] args) {
 		crear();
-	        
+	        leer();
+		actualizar(1,"NUEVO");
+		borrar(1);
 	}
 
 	public static void crear() {
@@ -28,6 +30,64 @@ public class MainApartado1 {
 	        } finally {
 	            em.close();
 	        }
+	}
+	public static void leer() {
+		EntityManager em = emf.createEntityManager();
+		try {
+            List<Continente> continentes = em.createQuery("SELECT c FROM Continente c", Continente.class).getResultList();
+            for (Continente c : continentes) {
+                System.out.println(c.getNombre());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            em.close();
+            
+        }
+	}
+	
+	public static void actualizar(int id, String nuevoNombre) {
+	    EntityManager em = emf.createEntityManager();
+	    try {
+	        em.getTransaction().begin();
+	        Continente continente = em.find(Continente.class, id);
+	        if (continente != null) {
+	            continente.setNombre(nuevoNombre);
+	            em.getTransaction().commit();
+	            System.out.println("Continente actualizado: " + continente.getNombre());
+	        } else {
+	            System.out.println("No se encontró el continente con ID: " + id);
+	            em.getTransaction().rollback();
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        em.getTransaction().rollback();
+	    } finally {
+	        em.close();
+	    }
+	}
+	
+	
+	public static void borrar(int id) {
+	    EntityManager em = emf.createEntityManager();
+	    try {
+	        em.getTransaction().begin();
+	        Continente continente = em.find(Continente.class, id);
+	        if (continente != null) {
+	            em.remove(continente);
+	            em.getTransaction().commit();
+	            System.out.println("Continente eliminado: " + continente.getNombre());
+	        } else {
+	            System.out.println("No se encontró el continente con ID: " + id);
+	            em.getTransaction().rollback();
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        em.getTransaction().rollback();
+	    } finally {
+	        em.close();
+	        emf.close();
+	    }
 	}
 	
 }
