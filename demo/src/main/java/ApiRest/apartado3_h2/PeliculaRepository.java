7a57b0910e3343482ee29e15e1fb7a9865167c9b
RicardoPQS <152377540+RicardoPQS@users.noreply.github.com>
2025-02-12 09:57:52 +0100

    Delete demo directory

--- demo/src/main/java/ApiRest/apartado3_h2/PeliculaRepository.java
+++
@@ -1,9 +0,0 @@
-package ApiRest.apartado3_h2;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PeliculaRepository extends JpaRepository<Pelicula, Long>{
-    
-}
